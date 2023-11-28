6e476ec8393dba8e98d282f7c52cf00d25347836
Aleksandr Ishutin <aleksandr.horosh93@gmail.com>
2023-11-28 22:13:44 +0400

    Создал новый Таск

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ function App() {
     let tasks1: Array<TaskType> = [
         { id: 1, title: 'HTML&CSS', isDone: true },
         { id: 2, title: 'JS', isDone: true },
-        { id: 2, title: 'React', isDone: false },
+        { id: 3, title: 'React', isDone: false },
+        { id: 4, title: 'Redux', isDone: false },
     ]
+    
     let tasks2: Array<TaskType> = [
-        { id: 1, title: 'Terminator', isDone: true },
-        { id: 2, title: 'XXX', isDone: false },
-        { id: 2, title: 'Jentlents of fortune', isDone: true },
+        { id: 1, title: 'Hello world', isDone: true },
+        { id: 2, title: 'I am Happy', isDone: false },
+        { id: 3, title: 'Yo', isDone: true },
     ]
     return (
         <div className="app">
             <Todolist tasks={tasks1} title='What to learn?' />
-            <Todolist tasks={tasks2} title='Movies' />
+            <Todolist tasks={tasks2} title='Songs' />
 
         </div>
     );
